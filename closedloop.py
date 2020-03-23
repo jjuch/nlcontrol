098630cf6d0204f62241470a3d9cf33e3b91c8df
Jasper Juchem <Jasper.Juchem@UGent.be>
2020-03-23 12:47:30 +0100

    Complete folder structure

---
+++ closedloop.py
@@ -0,0 +1,4 @@
+class ClosedLoop():
+    def __init__(self, system, controller):
+        self.system = system,
+        self.controller = controller
---
+++ controller.py
@@ -0,0 +1,5 @@
+from eula import EulerLagrange
+
+class Controller(EulerLagrange):
+    def __init__(self):
+        pass
