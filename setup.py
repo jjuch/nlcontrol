89cf3b83905c4f37e49503b9fa5dbbdb4a5fd2af
Jasper Juchem <Jasper.Juchem@UGent.be>
2020-04-11 15:43:37 +0200

    packaging files

---
+++ setup.py
@@ -0,0 +1,49 @@
+"""
+Nonlinear control toolbox, to work with symbolic nonlinear systems in a control context.
+
+See:
+https://github.ugent.be/jjuchem/nlcontrol
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get version
+exec(open('nlcontrol/version.py').read())
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='nlcontrol',
+    version=__version__,
+    description='A toolbox to simulate and analyse nonlinear systems in a control context.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='control systems nonlinear simulation',
+    url='https://github.ugent.be/jjuchem/nlcontrol',
+    author='Jasper Juchem',
+    author_email='Jasper.Juchem@UGent.be',
+    classifiers=[
+        'Development States :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience ::  Education',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3'
+    ],
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy>=1.17',
+        'scipy>=1.3',
+        'sympy>=1',
+        'control>=0.8',
+        'simupy>=1',
+        'matplotlib>=3'
+    ]
+)
