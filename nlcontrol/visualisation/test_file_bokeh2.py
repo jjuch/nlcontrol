25aa6c83b2e993caf4aabfe30aefdee3ed8bd9b3
Jasper Juchem <Jasper_Juchem@hotmail.com>
2020-11-30 10:47:08 +0100

    add summation

--- nlcontrol/visualisation/test_file_bokeh2.py
+++ nlcontrol/visualisation/test_file_bokeh2.py
@@ -1,8 +1,9 @@
-from bokeh.io import show, output_file, curdoc
+from bokeh.io import show, output_file, curdoc, save
 import bokeh.plotting as plt
 from bokeh.models import (Circle, ColumnDataSource, HoverTool, MultiLine, PanTool, Plot, Range1d, Rect, Text)
 
 import uuid
+import webbrowser
 
 font_size_in_pixels = 15
 
@@ -12,15 +13,13 @@ nodes = {
             'id': uuid.uuid4(),
             'label': 'system', 
             'pos': (2, 0.5),
-            'in_pos': (1.8, 0.5),
-            'out_pos': (2.2, 0.5) 
+            'direction': 'right'
             }, 
         2: {
             'id': uuid.uuid4(),
             'label': 'controller',
             'pos': (2, 0),
-            'in_pos': (2.2, 0),
-            'out_pos': (1.8, 0)
+            'direction': 'left'
             }
         },
     'summation': {
@@ -32,9 +31,6 @@ nodes = {
     }
     }
 
-# nodes_system = nodes['system']
-# for node in nodes_system:
-#     print('node = ', nodes_system[node])
 
 # Create Bokeh figure
 plot = Plot(plot_width=800, plot_height=700, x_range=Range1d(0.5, 2.5), y_range=Range1d(-0.5, 1))
@@ -42,12 +38,16 @@ plot = Plot(plot_width=800, plot_height=700, x_range=Range1d(0.5, 2.5), y_range=
 node_hover_tool = HoverTool(tooltips=[("(x,y)", "($x, $y)"),])
 plot.add_tools(PanTool(), node_hover_tool)
 
+
+# Conditions system nodes
 nodes_system = nodes['system']
 x = []
 y = []
 text = []
 width = []
 height = []
+in_coords = []
+out_coords = []
 
 # Add system text
 for node in nodes_system:
@@ -56,23 +56,48 @@ for node in nodes_system:
     y.append(cs['pos'][1])
     text.append(cs['label'])
     label_length = len(cs['label'])
-    # width.append('{}px'.format((label_length + 2) * 15))
-    # height.append('{}px'.format(font_size_in_pixels + 2))
-    width.append(0.3)
+    width.append(0.03 * (label_length + 2))
     height.append(0.1)
+    left_coord = (x[-1] - width[-1] / 2, y[-1])
+    right_coord = (x[-1] + width[-1] / 2, y[-1])
+    if cs['direction'] == 'right':
+        cs['in_pos'] = left_coord
+        cs['out_pos'] = right_coord
+    else:
+        cs['in_pos'] = right_coord
+        cs['out_pos'] = left_coord
 
-print(width)
-print(height)
+source_systems = ColumnDataSource(dict(x=x, y=y, text=text, width=width, height=height))
 
-source = ColumnDataSource(dict(x=x, y=y, text=text, width=width, height=height))
+# Condition sum nodes
+nodes_system = nodes['summation']
+x = []
+y = []
+text = []
+diameter = []
+in_coords = []
+out_coords = []
+
+# Add system text
+for node in nodes_system:
+    cs = nodes_system[node]
+    x.append(cs['pos'][0])
+    y.append(cs['pos'][1])
+    text.append(cs['label'])
+    label_length = len(cs['label'])
+    diameter.append(0.03 * (label_length + 2))
+    in_coord = (x[-1], y[-1] - diameter[-1] / 2)
+    out_coord = (x[-1] + diameter[-1] / 2, y[-1])
 
 # Add system box
 glyph_box = Rect(x="x", y="y", width="width", height="height", fill_color="#cab2d6")
-plot.add_glyph(source, glyph_box)
+plot.add_glyph(source_systems, glyph_box)
 
 glyph_text = Text(x='x', y='y', text='text', text_font_size='{}px'.format(font_size_in_pixels), text_color="#000000", text_baseline="middle", text_align="center")
-plot.add_glyph(source, glyph_text)
+plot.add_glyph(source_systems, glyph_text)
+
 
 
 curdoc().add_root(plot)
-show(plot)
+output_file("interactive_graphs2.html", 'Block scheme')
+show(plot, browser='windows-default', new='window')
