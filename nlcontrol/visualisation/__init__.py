500e362d68476d76e1cdf6cad0fcc296e751553e
Jasper Juchem <Jasper_Juchem@hotmail.com>
2020-12-02 11:19:18 +0100

    init oop visualisation

---
+++ nlcontrol/visualisation/__init__.py
@@ -0,0 +1,16 @@
+"""
+VISUALISATION LIBRARY (:mod: `nlcontrol.visualisation')
+=======================================================
+
+.. currentmodule:: nlcontrol.visualisation
+
+Classes:
+    * base:
+        RendererBase : base class for visuals renderer
+
+Functions:
+
+
+"""
+
+from .base import RendererBase
